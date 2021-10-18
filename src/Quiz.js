eb5d1b9db5d5268a4e7e8ee15d9d964d0041eb29
tjlinton <tj_linton@yahoo.com>
2021-10-18 09:16:19 -0500

    Fixed props issue

--- src/Quiz.js
+++ src/Quiz.js
@@ -2,9 +2,9 @@ import React, { Component } from "react";
 
 let quizData = require("./quiz_data.json");
 
-class Quiz extends React.Component {
-  constructor() {
-    super(this.props);
+class Quiz extends Component {
+  constructor(props) {
+    super(props);
 
     this.state = {
       quiz_position: 1,
